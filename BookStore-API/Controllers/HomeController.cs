29645fc706affe125197d98e05fcda252bf4c418
onunpb <nunoapbarros@gmail.com>
2020-11-09 22:28:34 +0000

    Add project files.

---
+++ BookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NLog.Fluent;
+
+namespace BookStore_API.Controllers
+{
+    /// <summary>
+    /// This is a test API Controller
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.ControllerBase" />
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILoggerService _logger;
+
+        public HomeController(ILoggerService logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Gets Values
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/Home
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Accessed Home Controller");
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Gets a value 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET: api/Home/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            _logger.LogDebug("Got a Value");
+            return "value";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        // POST: api/Home
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _logger.LogError("This is an error");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        // PUT: api/Home/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _logger.LogWarn("This is a warning");
+        }
+    }
+}
