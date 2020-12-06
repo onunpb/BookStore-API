68a9c6d4b6bd6428e8139c12888050259d378318
onunpb <nunoapbarros@gmail.com>
2020-12-06 13:27:05 +0000

    Configuration of the Base repository

--- BookStore-UI/Contracts/IBaseRepository.cs
+++ BookStore-UI/Contracts/IBaseRepository.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace BookStore_UI.Contracts
 {
-    public interface IBaseRepository
+    public interface IBaseRepository<T> where T : class
     {
+        Task<T> Get(string url, int id);
+
+        Task<IList<T>> Get(string url);
+
+        Task<bool> Create(string url, T obj);
+
+        Task<bool> Update(string url, T obj);
+
+        Task<bool> Delete(string url, int id);
     }
 }
