d4afaae3acd2fb6098eab975feb0ee9441731e5b
onunpb <nunoapbarros@gmail.com>
2020-12-06 14:55:15 +0000

    Setup HTTP Client and Service Repositories.

---
+++ BookStore-UI/Static/Endpoints.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStore_UI.Static
+{
+    public static class Endpoints
+    {
+        public static string BaseUrl = "https://localhost:44334/";
+        public static string AuthorsEndpoint = $"{BaseUrl}api/authors/";
+        public static string BooksEndpoint = $"{BaseUrl}api/books/";
+    }
+}
