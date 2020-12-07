eab531002ecd57a81c25b4c632493bf160319c40
onunpb <nunoapbarros@gmail.com>
2020-12-07 14:54:51 +0000

    Setup User Registration Form.

--- BookStore-API/Controllers/UsersController.cs
+++ BookStore-API/Controllers/UsersController.cs
@@ -36,12 +36,46 @@ namespace BookStore_API.Controllers
             _config = config;
         }
 
+        /// <summary>
+        /// User Registration Endpoint.
+        /// </summary>
+        /// <param name="userDTO">The user dto.</param>
+        /// <returns></returns>
+        [Route("register")]
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] UserDTO userDTO)
+        {
+            var location = GetControllerActionNames();
+            try
+            {
+                var username = userDTO.EmailAddress;
+                var password = userDTO.Password;
+                _logger.LogInfo($"{location}: Registration Attempt for {username}");
+                var user = new IdentityUser { Email = username, UserName = username };
+                var result = await _userManager.CreateAsync(user, password);
+
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        _logger.LogError($"{location}: {error.Code} {error.Description}");
+                    }
+                    return InternalError($"{location}: {username} User Registration Attempt Failed");
+                }
+                return Ok(new { result.Succeeded });
+            }
+            catch(Exception e)
+            {
+                return InternalError($"{location}: {e.Message} - {e.InnerException}");
+            }
+        }
 
         /// <summary>
         /// User Login Endpoint.
         /// </summary>
         /// <param name="userDTO">The user dto.</param>
         /// <returns></returns>
+        [Route("login")]
         [AllowAnonymous]
         [HttpPost]
         public async Task<IActionResult> Login([FromBody] UserDTO userDTO)
@@ -49,7 +83,7 @@ namespace BookStore_API.Controllers
             var location = GetControllerActionNames();
             try
             {
-                var username = userDTO.Username;
+                var username = userDTO.EmailAddress;
                 var password = userDTO.Password;
                 _logger.LogInfo($"{location}: Login Attempt from user {username}");
                 var result = await _singnInManager.PasswordSignInAsync(username, password, false, false);
