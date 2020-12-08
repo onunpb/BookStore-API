6362b7dbe2f8583e287980b6bd8c6c3a4923ec40
onunpb <nunoapbarros@gmail.com>
2020-12-08 15:43:37 +0000

    Setup User Registration Form

--- BookStore-UI/Startup.cs
+++ BookStore-UI/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BookStore_UI.Data;
+using BookStore_UI.Contracts;
+using BookStore_UI.Service;
 
 namespace BookStore_UI
 {
@@ -28,7 +30,8 @@ namespace BookStore_UI
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddHttpClient();
+            services.AddTransient<IAuthenticationRepository,AuthenticationRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- BookStore-UI/Static/Endpoints.cs
+++ BookStore-UI/Static/Endpoints.cs
@@ -5,5 +5,6 @@
         public static string BaseUrl = "https://localhost:44334/";
         public static string AuthorsEndpoint = $"{BaseUrl}api/authors/";
         public static string BooksEndpoint = $"{BaseUrl}api/books/";
+        public static string RegisterEndpoint = $"{BaseUrl}api/users/register";
     }
 }
